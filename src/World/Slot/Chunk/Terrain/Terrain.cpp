ad4f74fca5f29ae14922d5bf0abd081a03624970
edherbert <edward@othermythos.com>
2019-09-04 20:14:10 +0100

    Started work on the improved terrain resource system.

--- src/World/Slot/Chunk/Terrain/Terrain.cpp
+++ src/World/Slot/Chunk/Terrain/Terrain.cpp
@@ -4,29 +4,24 @@
 
 #include "System/SystemSetup/SystemSettings.h"
 
+#include "filesystem/path.h"
+
 #include <Ogre.h>
+#include "Logger/Log.h"
 
 namespace AV{
-    Terrain::Terrain(Ogre::SceneNode* n){
+    Terrain::Terrain(){
 
         Ogre::Root* root = Ogre::Root::getSingletonPtr();
         Ogre::SceneManager* mgr = root->getSceneManager("Scene Manager");
 
+        //In future this might be moved to setup.
         mTerra = new Ogre::Terra( Ogre::Id::generateNewId<Ogre::MovableObject>(),
                                         &mgr->_getEntityMemoryManager( Ogre::SCENE_STATIC ),
                                         mgr, 0, Ogre::Root::getSingletonPtr()->getCompositorManager2(),
                                         mgr->getCameras()[0]);
 
         mTerra->setCastShadows( false );
-
-        Ogre::Vector3 nPos = n->getPosition();
-        int slotSize = SystemSettings::getWorldSlotSize();
-        mTerra->load( "Heightmap.png", Ogre::Vector3(nPos.x, 0, nPos.z), Ogre::Vector3(slotSize, slotSize, slotSize));
-
-        Ogre::HlmsDatablock *datablock = root->getHlmsManager()->getDatablock( "TerraExampleMaterial" );
-        mTerra->setDatablock( datablock );
-
-        n->attachObject( mTerra );
     }
 
     Terrain::~Terrain(){
@@ -37,6 +32,64 @@ namespace AV{
         //From the looks of it, the second value isn't used for anything other than determining whether the shadow map should be updated.
         //It's updated once every frame if the value is set to 0.
         //Setting it to 1 means the shadow map should only update once on creation, and I guess you could dynamically adjust this value depending on when you want to update it.
-        mTerra->update( Ogre::Vector3( -1, -1, -1 ).normalisedCopy(), 1);
+        if(mChunkHasTerrain){
+            mTerra->update( Ogre::Vector3( -1, -1, -1 ).normalisedCopy(), 1);
+        }
+    }
+
+    void Terrain::_createTerrainResourceGroup(const Ogre::String& dirPath, const Ogre::String& groupName){
+        Ogre::ResourceGroupManager::getSingleton().createResourceGroup(groupName, false);
+        Ogre::Root::getSingleton().addResourceLocation(dirPath, "FileSystem", groupName); //The resource group is the coordinates of this chunk.
+
+        Ogre::ResourceGroupManager::getSingleton().initialiseResourceGroup(groupName, false);
+    }
+
+    void Terrain::provideSceneNode(Ogre::SceneNode* node){
+        //In future I plan to have the scene node be passed in at setup.
+        //Terrain instances and instances of terra are going to be recycled between chunks, so setup is going to try and reduce the ammount of stuff it does.
+
+        mNode = node;
+    }
+
+    void Terrain::setup(const ChunkCoordinate& coord){
+        assert(mNode);
+
+        const Ogre::String chunkPathString(SystemSettings::getMapsDirectory() + "/" + coord.getFilePath() + "/terrain");
+        //Within the chunk path there should be a directory named terrain. If this doesn't exist then this chunk has no terrain.
+        const filesystem::path chunkPath(chunkPathString);
+        if(!chunkPath.exists()) return;
+        if(!chunkPath.is_directory()) return;
+
+        const filesystem::path heightPath = chunkPath / filesystem::path("height.png");
+        if(!heightPath.exists() || !heightPath.is_file()) return; //The only file we absolutely need to enable the terrain is the heightmap.
+
+
+        //Everything is in place, so start with the terrain.
+        mTerrainGroupName = coord.getTerrainGroupName();
+
+        //If that directory exists, create a new resource group within it. This will allow us to safely collect all the resources specific to that directory.
+        _createTerrainResourceGroup(chunkPathString, mTerrainGroupName);
+
+        Ogre::Image img;
+        img.load("height.png", mTerrainGroupName);
+
+        AV_ERROR("Loaded image");
+
+
+
+
+        const Ogre::Vector3 nPos = mNode->getPosition();
+        int slotSize = SystemSettings::getWorldSlotSize();
+        mTerra->load( img, Ogre::Vector3(nPos.x, 0, nPos.z), Ogre::Vector3(slotSize, slotSize, slotSize));
+
+        Ogre::Root* root = Ogre::Root::getSingletonPtr();
+        Ogre::HlmsDatablock *datablock = root->getHlmsManager()->getDatablock( "TerraExampleMaterial" );
+        //Seems you have to set the datablock after the load.
+        //Otherwise when you try and set it, it misses the renderables because they don't exist.
+        mTerra->setDatablock( datablock );
+
+        mNode->attachObject( mTerra );
+
+        mChunkHasTerrain = true;
     }
 }
--- src/World/Slot/Chunk/Terrain/Terrain.h
+++ src/World/Slot/Chunk/Terrain/Terrain.h
@@ -5,15 +5,33 @@ namespace Ogre{
     class SceneNode;
 }
 
+#include "World/Slot/ChunkCoordinate.h"
+#include "OgreString.h"
+
 namespace AV{
     class Terrain{
     public:
-        Terrain(Ogre::SceneNode* n);
+        Terrain();
         ~Terrain();
 
         void update();
+        /**
+        Setup the terrain, letting it know which chunk it represents.
+        */
+        void setup(const ChunkCoordinate& coord);
+
+        //TODO Temporary.
+        void provideSceneNode(Ogre::SceneNode* node);
 
     private:
         Ogre::Terra* mTerra;
+        Ogre::SceneNode* mNode = 0;
+
+        bool mChunkHasTerrain = false;
+        Ogre::String mTerrainGroupName;
+
+        ChunkCoordinate mCurrentChunk;
+
+        void _createTerrainResourceGroup(const Ogre::String& dirPath, const Ogre::String& groupName);
     };
 }
