7ef075194ab2152b2d006a37eed6447e17bc7aa7
edherbert <edward@othermythos.com>
2020-06-12 21:28:15 +0100

    Updated the camera namespace to allow the new vector3 and slot positions.

--- src/Scripting/ScriptNamespace/CameraNamespace.cpp
+++ src/Scripting/ScriptNamespace/CameraNamespace.cpp
@@ -3,31 +3,60 @@
 #include "OgreCamera.h"
 #include "OgreVector3.h"
 
+#include "Scripting/ScriptNamespace/Classes/Vector3UserData.h"
+#include "Scripting/ScriptNamespace/Classes/SlotPositionClass.h"
+
 namespace AV{
     Ogre::Camera* CameraNamespace::_camera = 0;
 
+    bool _vec3Read(HSQUIRRELVM vm, Ogre::Vector3& outVec){
+        SQInteger size = sq_gettop(vm);
+
+        if(size == 2){
+            //Vector3
+            if(Vector3UserData::readVector3FromUserData(vm, -1, &outVec)) return true;
+
+            //TODO there should be error checking here.
+            SlotPosition pos = SlotPositionClass::getSlotFromInstance(vm, -1);
+            outVec = pos.toOgre();
+
+            //return false;
+        }else if(size == 4){
+            //Regular
+
+            bool success = true;
+            SQFloat x, y, z;
+            success &= SQ_SUCCEEDED(sq_getfloat(vm, -1, &z));
+            success &= SQ_SUCCEEDED(sq_getfloat(vm, -2, &y));
+            success &= SQ_SUCCEEDED(sq_getfloat(vm, -3, &x));
+            if(!success) return false;
+
+            outVec = Ogre::Vector3(x, y, z);
+
+            sq_pop(vm, 3);
+        }else return false;
+
+        return true;
+    }
+
     SQInteger CameraNamespace::setCameraPosition(HSQUIRRELVM vm){
-        SQFloat x, y, z;
-        sq_getfloat(vm, -1, &z);
-        sq_getfloat(vm, -2, &y);
-        sq_getfloat(vm, -3, &x);
+        Ogre::Vector3 target;
+        if(!_vec3Read(vm, target)){
+            return 0;
+        }
 
-        _camera->setPosition(Ogre::Vector3(x, y, z));
-
-        sq_pop(vm, 3);
+        _camera->setPosition(target);
 
         return 0;
     }
 
     SQInteger CameraNamespace::cameraLookat(HSQUIRRELVM vm){
-        SQFloat x, y, z;
-        sq_getfloat(vm, -1, &z);
-        sq_getfloat(vm, -2, &y);
-        sq_getfloat(vm, -3, &x);
+        Ogre::Vector3 target;
+        if(!_vec3Read(vm, target)){
+            return 0;
+        }
 
-        _camera->lookAt(Ogre::Vector3(x, y, z));
-
-        sq_pop(vm, 3);
+        _camera->lookAt(target);
 
         return 0;
     }
@@ -44,7 +73,17 @@ namespace AV{
         @param2:y: y position.
         @param3:z: x position.
         */
-        ScriptUtils::addFunction(vm, setCameraPosition, "setPosition", 4, ".nnn");
+        /**SQFunction
+        @name setPosition
+        @desc Set the position of the camera.
+        @param1:Vec3: Vector representing the position in 3d space.
+        */
+        /**SQFunction
+        @name setPosition
+        @desc Set the position of the camera.
+        @param1:SlotPosition: SlotPosition representing the position in world coordinates. This will be resolved to local coordinates automatically.
+        */
+        ScriptUtils::addFunction(vm, setCameraPosition, "setPosition", -2, ".n|u|xnn");
         /**SQFunction
         @name lookAt
         @desc Point the camera towards a specific coordinate.
@@ -52,6 +91,16 @@ namespace AV{
         @param2:y: Point y position.
         @param3:z: Point x position.
         */
-        ScriptUtils::addFunction(vm, cameraLookat, "lookAt", 4, ".nnn");
+        /**SQFunction
+        @name lookAt
+        @desc Point the camera towards a specific coordinate.
+        @param1:Vec3: Vector representing the position to look at.
+        */
+        /**SQFunction
+        @name lookAt
+        @desc Point the camera towards a specific coordinate.
+        @param1:SlotPosition: SlotPosition representing the position in world coordinates. This will be resolved to local coordinates automatically.
+        */
+        ScriptUtils::addFunction(vm, cameraLookat, "lookAt", -2, ".n|u|xnn");
     }
 }
