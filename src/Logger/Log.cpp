edfc8139f54990fc2dad48239de0cb1aacf46a05
edherbert <edward@othermythos.com>
2020-10-18 11:38:46 +0100

    Changed the logger class to log to the windows console when in console mode.

--- src/Logger/Log.cpp
+++ src/Logger/Log.cpp
@@ -1,9 +1,13 @@
 #include "Log.h"
 
 #ifdef WIN32
-	#include "spdlog/sinks/msvc_sink.h"
+    #ifdef WIN_DESKTOP_APPLICATION
+        #include "spdlog/sinks/msvc_sink.h"
+    #else
+        #include "spdlog/sinks/stdout_color_sinks.h"
+    #endif
 #else
-	#include "spdlog/sinks/stdout_color_sinks.h"
+    #include "spdlog/sinks/stdout_color_sinks.h"
 #endif
 
 
@@ -11,12 +15,17 @@ namespace AV {
     std::shared_ptr<spdlog::logger> Log::_logger;
     
     void Log::Init(){
-		#ifdef WIN32
-			auto sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
-			_logger = std::make_shared<spdlog::logger>("AV", sink);
-		#else
-			_logger = spdlog::stdout_color_mt("AV");
-		#endif
+        #ifdef WIN32
+            #ifdef WIN_DESKTOP_APPLICATION
+                auto sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
+                _logger = std::make_shared<spdlog::logger>("AV", sink);
+            #else
+                _logger = spdlog::stdout_color_mt("AV");
+            #endif
+
+        #else
+            _logger = spdlog::stdout_color_mt("AV");
+        #endif
 
         _logger->set_level(spdlog::level::trace);
     }
