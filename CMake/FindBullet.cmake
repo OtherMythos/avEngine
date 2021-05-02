b4edabe9505f126ba78ca5440ade101b31fcc8f9
edherbert <edward@othermythos.com>
2021-05-03 00:04:48 +0100

    Added bullet and other builds.

---
+++ CMake/FindBullet.cmake
@@ -0,0 +1,41 @@
+
+FIND_PATH(Bullet_INCLUDE_DIR Bullet3Common
+    PATHS ${Bullet_ROOT}/src
+    ENV Bullet_ROOT)
+
+FIND_LIBRARY(BULLET_LINEARMATH NAMES LinearMath
+    PATHS ${Bullet_ROOT}/build/${CMAKE_BUILD_TYPE}/src/LinearMath
+    PATH_SUFFIXES a
+)
+FIND_LIBRARY(BULLET_COLLISION NAMES BulletCollision
+    PATHS ${Bullet_ROOT}/build/${CMAKE_BUILD_TYPE}/src/BulletCollision
+    PATH_SUFFIXES a
+)
+FIND_LIBRARY(BULLET_DYNAMICS NAMES BulletDynamics
+    PATHS ${Bullet_ROOT}/build/${CMAKE_BUILD_TYPE}/src/BulletDynamics
+    PATH_SUFFIXES a
+)
+
+if(BULLET_LINEARMATH)
+    set(Bullet_LIBRARY "${Bullet_LIBRARY};${BULLET_LINEARMATH};${BULLET_COLLISION};${BULLET_DYNAMICS}" CACHE STRING "" FORCE)
+endif()
+
+SET(Bullet_FOUND FALSE)
+IF(Bullet_INCLUDE_DIR AND Bullet_LIBRARY)
+    SET(Bullet_FOUND TRUE)
+ENDIF(Bullet_INCLUDE_DIR AND Bullet_LIBRARY)
+
+IF(Bullet_FOUND)
+    IF (NOT Bullet_FIND_QUIETLY)
+        MESSAGE(STATUS "Found bullet: ${Bullet_LIBRARY}")
+    ENDIF (NOT Bullet_FIND_QUIETLY)
+        ELSE(Bullet_FOUND)
+    IF (Bullet_FIND_REQUIRED)
+        MESSAGE(FATAL_ERROR "Could not find bullet")
+    ENDIF (Bullet_FIND_REQUIRED)
+ENDIF(Bullet_FOUND)
+
+mark_as_advanced( FORCE
+    Bullet_INCLUDE_DIR
+    Bullet_LIBRARY
+)
