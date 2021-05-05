c441b862fa5885bc0d0cc11f1796fe213999254e
edherbert <edward@othermythos.com>
2021-05-05 22:46:43 +0100

    Added support for building the unit tests.

---
+++ CMake/FindGoogleTest.cmake
@@ -0,0 +1,46 @@
+
+
+FIND_PATH(GOOGLE_TEST_INCLUDE_DIR gtest
+    PATHS ${GoogleTest_ROOT}
+        ENV GoogleTest_ROOT
+    PATH_SUFFIXES googletest/include)
+
+FIND_PATH(GOOGLE_MOCK_INCLUDE_DIR gmock
+    PATHS ${GoogleTest_ROOT}
+        ENV GoogleTest_ROOT
+    PATH_SUFFIXES googlemock/include)
+
+FIND_LIBRARY(GoogleTest_MOCK_LIBRARY NAMES gmock gmockd
+    PATHS ${GoogleTest_ROOT}/build/${CMAKE_BUILD_TYPE}/lib
+        ENV GoogleTest_ROOT
+    PATH_SUFFIXES lib)
+
+FIND_LIBRARY(GoogleTest_TEST_LIBRARY NAMES gtest gtestd
+    PATHS ${GoogleTest_ROOT}/build/${CMAKE_BUILD_TYPE}/lib
+        ENV GoogleTest_ROOT
+    PATH_SUFFIXES lib)
+
+set(GoogleTest_LIBRARY "${GoogleTest_LIBRARY};${GoogleTest_MOCK_LIBRARY};${GoogleTest_TEST_LIBRARY}"
+CACHE STRING "" FORCE)
+set(GoogleTest_INCLUDE_DIR "${GoogleTest_INCLUDE_DIR};${GOOGLE_TEST_INCLUDE_DIR};${GOOGLE_MOCK_INCLUDE_DIR}"
+CACHE STRING "" FORCE)
+
+SET(GoogleTest_FOUND FALSE)
+IF(GoogleTest_INCLUDE_DIR AND GoogleTest_LIBRARY)
+   SET(GoogleTest_FOUND TRUE)
+ENDIF(GoogleTest_INCLUDE_DIR AND GoogleTest_LIBRARY)
+
+IF(GoogleTest_FOUND)
+   IF (NOT GoogleTest_FIND_QUIETLY)
+	   MESSAGE(STATUS "Found GoogleTest")
+   ENDIF (NOT GoogleTest_FIND_QUIETLY)
+ELSE(GoogleTest_FOUND)
+   IF (GoogleTest_FIND_REQUIRED)
+      MESSAGE(FATAL_ERROR "Could not find GoogleTest")
+   ENDIF (GoogleTest_FIND_REQUIRED)
+ENDIF(GoogleTest_FOUND)
+
+mark_as_advanced( FORCE
+    GoogleTest_INCLUDE_DIR
+    GoogleTest_LIBRARY
+)
