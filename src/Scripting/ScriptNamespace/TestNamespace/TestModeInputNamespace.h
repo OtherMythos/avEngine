9f6208d11730c0296d770e5a8d639ff3414ae9c7
edherbert <edward@othermythos.com>
2022-06-28 22:22:22 +0100

    Removed some unused test input functions.

--- src/Scripting/ScriptNamespace/TestNamespace/TestModeInputNamespace.h
+++ src/Scripting/ScriptNamespace/TestNamespace/TestModeInputNamespace.h
@@ -12,11 +12,6 @@ namespace AV{
         static void setupTestNamespace(HSQUIRRELVM vm, SQFUNCTION messageFunction, bool testModeEnabled);
 
     private:
-        static SQInteger sendButtonAction(HSQUIRRELVM vm);
-        static SQInteger sendTriggerAction(HSQUIRRELVM vm);
-        static SQInteger sendAxisAction(HSQUIRRELVM vm);
-        static SQInteger sendKeyboardKeyPress(HSQUIRRELVM vm);
-
         static SQInteger sendControllerInput(HSQUIRRELVM vm);
     };
 };
