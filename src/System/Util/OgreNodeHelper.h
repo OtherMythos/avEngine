22e5582b662a9b393c01a457ddbe9f522822dda3
edherbert <edward@othermythos.com>
2024-02-07 18:14:09 +0000

    Fixed an issue with node destruction.

--- src/System/Util/OgreNodeHelper.h
+++ src/System/Util/OgreNodeHelper.h
@@ -26,12 +26,10 @@ namespace AV{
         static void destroyMovableObject(Ogre::SceneNode* node){
             //When it comes time to destroy a mesh (movable object) a bit more work needs to be done, as you have to check it's actually there.
             Ogre::SceneNode::ObjectIterator it = node->getAttachedObjectIterator();
-            if(it.hasMoreElements()){
+            while(it.hasMoreElements()){
                 Ogre::MovableObject* obj = it.getNext();
                 node->getCreator()->destroyMovableObject(obj);
             }
-            //At the moment a mesh object should only have the one mesh attached to it, so by this point there should be no more.
-            assert(!it.hasMoreElements());
         }
 
         /**
