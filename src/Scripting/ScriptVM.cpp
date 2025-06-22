00e062f54d8eb6052657dd3ffad16aa412b4e472
edherbert <edward@othermythos.com>
2025-06-22 19:58:46 +0100

    Fix for squirrel functions which were not linked.

--- src/Scripting/ScriptVM.cpp
+++ src/Scripting/ScriptVM.cpp
@@ -384,6 +384,12 @@ namespace AV {
         //Setup the root table.
         sq_pushroottable(vm);
 
+        {
+            //Call these squirrel functions so the linker doesn't remove them.
+            void* dummy = (void*) &sq_move;
+            void* dummySecond = (void*) &sq_set;
+        }
+
         sqstd_register_mathlib(vm);
         //sqstd_register_systemlib(vm);
         //sqstd_register_iolib(vm);
