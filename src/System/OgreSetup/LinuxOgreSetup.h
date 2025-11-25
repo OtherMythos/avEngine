1fc1a437b7712b7d45be349f441bcc3f3301d17e
edherbert <edward@othermythos.com>
2025-11-25 09:35:37 +0000

    Altered Linux setup to only load the required render system.

--- src/System/OgreSetup/LinuxOgreSetup.h
+++ src/System/OgreSetup/LinuxOgreSetup.h
@@ -27,30 +27,32 @@ namespace AV{
         Ogre::Root* setupRoot(){
             Ogre::Root *root = _setupBasicOgreRoot();
 
+            Ogre::String targetRenderSystem;
+            SystemSettings::RenderSystemTypes system = SystemSettings::getCurrentRenderSystem();
+            switch (system) {
+                case SystemSettings::RenderSystemTypes::RENDER_SYSTEM_VULKAN:
+                    targetRenderSystem = "RenderSystem_Vulkan";
+                    break;
+                default:
+                case SystemSettings::RenderSystemTypes::RENDER_SYSTEM_OPENGL:
+                    targetRenderSystem = "RenderSystem_GL3Plus";
+                    break;
+            }
+
             //I've found that in Linux (specifically mint), you need to have an absolute path to the shared object.
             //For some reason you can just name it in arch.
             //I'd say that the buck probably stops with ogre on that one, and it's doing something different per distribution, but that's just a hypothesis. I haven't checked any source code.
             //However I might as well just do an absolute path on all distributions, it'll just make it more reliable at the end of the day.
             //You can just sym link to the libs if necessary, but they now need to be in the master directory.
             filesystem::path masterPath(SystemSettings::getMasterPath());
-            filesystem::path renderSystemPath = masterPath / filesystem::path("RenderSystem_GL3Plus.so");
-            filesystem::path renderSystemVulkanPath = masterPath / filesystem::path("RenderSystem_Vulkan.so");
-            filesystem::path particleFXPath = masterPath / filesystem::path("Plugin_ParticleFX.so");
 
+            filesystem::path renderSystemPath = masterPath / filesystem::path(targetRenderSystem + ".so");
             root->loadPlugin(renderSystemPath.str(), false, 0);
-            root->loadPlugin(renderSystemVulkanPath.str(), true, 0);
+
+            filesystem::path particleFXPath = masterPath / filesystem::path("Plugin_ParticleFX.so");
             root->loadPlugin(particleFXPath.str(), false, 0);
 
-            size_t arrayIdx = 0;
-            SystemSettings::RenderSystemTypes renderSystem = SystemSettings::getCurrentRenderSystem();
-            if(renderSystem == SystemSettings::RenderSystemTypes::RENDER_SYSTEM_OPENGL){
-                arrayIdx = 0;
-            }
-            else if(renderSystem == SystemSettings::RenderSystemTypes::RENDER_SYSTEM_VULKAN){
-                arrayIdx = 1;
-            }
-
-            root->setRenderSystem(root->getAvailableRenderers()[arrayIdx]);
+            root->setRenderSystem(root->getAvailableRenderers()[0]);
             root->initialise(false);
 
             return root;
